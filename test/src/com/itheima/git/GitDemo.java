993be5c8e6d8ec1e05f70354f59ce6e72243d148
Eminem <pyduxintang@qq.com>
2019-06-30 19:12:20 +0800

    提交git

---
+++ test/src/com/itheima/git/GitDemo.java
@@ -0,0 +1,14 @@
+package com.itheima.git;
+
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("段新嘉真菜");
+        System.out.println("段新嘉真菜");
+        System.out.println("段新嘉真菜");
+        System.out.println("段新嘉真菜");
+    }
+    public void test(){
+        int i = 0;
+        i += 1;
+    }
+}
